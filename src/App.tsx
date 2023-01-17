7451c2c1a7f3a4ec9da5d253bc9253aedd0a32e6
khaled39ab <khaled39ab@gmail.com>
2023-01-17 18:33:43 +0600

    props type object practice

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import './App.css';
 // import User1 from './components/User1';
 import User2 from './components/User2';
 
+const user2 = {
+  name: 'Lamia',
+  age: 18,
+  email: 'lamia02@gmail.com',
+  isStudent: true,
+  lang: ['Bangla', 'English', 'Hindi']
+}
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +18,8 @@ function App() {
       <h2>User Registration</h2>
       {/* <User1 user={"Rimi"} age={25} isRegistered={true} />
       <User1 user={"Sazid"} age={26} isRegistered={false} /> */}
-      <User2 name={"Sazid"} age={26} isStudent={false} lang ={['Bangla', 'English']}/>
+      {/* <User2 name={"Sazid"} age={26} isStudent={false} lang ={['Bangla', 'English']}/> */}
+      <User2 user = {user2}/>
     </div>
   );
 }
--- src/components/User2.tsx
+++ src/components/User2.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/* 
 type userProps = {
     name: string,
     age: number,
@@ -22,5 +23,36 @@ const User2 = ({ name, age, isStudent, lang }: userProps) => {
         </div>
     );
 };
+
+*/
+
+type userProps = {
+    user: {
+        name: string;
+        age: number;
+        email: string;
+        isStudent: boolean;
+        lang: string[]
+    }
+}
+
+const User2 = ({ user }: userProps) => {
+    const { name, age, email, isStudent, lang } = user;
+
+    return (
+        <div style={{ border: '1px solid', margin: '2rem' }}>
+            <h3>User: {name}</h3>
+            <p>{age} years old.</p>
+            <p>Email: {email}</p>
+            {
+                isStudent ? <p>Student</p> : <p>Not Student</p>
+            }
+            {
+               <p> {lang.map((lan, index) => <span key={index}>{lan +' '}</span>)} </p>
+            }
+        </div>
+    );
+};
+
 
 export default User2;
