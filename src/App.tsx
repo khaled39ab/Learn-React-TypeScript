e9560ad1c9b3d906583f7e3272a4b837818f5a17
khaled39ab <khaled39ab@gmail.com>
2023-01-17 15:52:47 +0600

    props type practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import User from './components/User';
 
 function App() {
   return (
     <div className="App">
       <h1>Learn React TypeScript</h1>
+      <h2>User Registration</h2>
+      <User user={"Rimi"} age={25} isRegistered={true} />
+      <User user={"Sazid"} age={26} isRegistered={false} />
     </div>
   );
 }
---
+++ src/components/User.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// const User = (props: {user: string, age: number}) => {
+// const User = ({ user, age, isRegistered }: { user: string, age: number, isRegistered: boolean }) => {
+type userProps = {
+    user: string,
+    age: number,
+    isRegistered: boolean
+}
+const User = ({ user, age, isRegistered }: userProps) => {
+    return (
+        <div style={{ border: '2px solid pink', margin: '2rem' }}>
+            <h3>{user}</h3>
+            <p>{age} years old</p>
+            {isRegistered ? <p>Registered person</p> : <p>Not registered</p>}
+        </div>
+    );
+};
+
+export default User;
