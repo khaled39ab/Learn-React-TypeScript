bba65151052abf2dda63343fcce43ec52174b8da
khaled39ab <khaled39ab@gmail.com>
2023-01-19 23:18:33 +0600

    useState hook's type

--- src/components/useState hook/HookType.tsx
+++ src/components/useState hook/HookType.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
+type User = {
+    id: number,
+    name: string
+}
+
 const HookType = () => {
     const [count, setCount] = useState(0);
     // const [count, setCount] = useState<number>(0);  // no need to set type
 
+    const [users, setUsers] = useState<null | User>(null);
 
     const handleIncrease = () => {
         // setCount( "1")  // Error: Argument of type 'string' is not assignable to parameter of type 'SetStateAction<number>'
@@ -15,6 +21,10 @@ const HookType = () => {
         setCount(count - 1)
     }
 
+    const handleAddUser = () => {
+        setUsers({ id: 1, name: "Aminul" })
+    }
+
     return (
         <div>
             <h2>useState Hook Type</h2>
@@ -22,6 +32,11 @@ const HookType = () => {
                 <h3>Count: {count}</h3>
                 <button onClick={handleIncrease}>Increase</button>
                 <button onClick={handleDecrease}>Increase</button>
+            </div>
+
+            <div style={{ border: '2px solid pink', margin: '2rem', paddingBottom: '1.5rem' }}>
+                <h3>{users?.name}</h3>
+                <button onClick={handleAddUser}>Add user</button>
             </div>
         </div>
     );
