06ee4a8067b00744bdc9ac7671558c1ae8912739
khaled39ab <khaled39ab@gmail.com>
2023-01-20 00:17:26 +0600

    event type set with react.mouseEvent

--- src/components/events type/NewUser.tsx
+++ src/components/events type/NewUser.tsx
@@ -2,18 +2,35 @@ import React, { useState } from 'react';
 
 const NewUser = () => {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
 
     const handleUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUsername(e.target.value)
     }
-    
+
+    const handleSetTitle = (e: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(username);
+    }
+
+    const handleAddUser = (e: React.FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setEmail(e.currentTarget.value)
+    }
+
     return (
         <div>
             <h2>Event Type</h2>
             <h3>{username}</h3>
+            <h3>{email}</h3>
             <div style={{ border: '2px solid pink', margin: '2rem', paddingBottom: '1.5rem' }}>
                 <h3>Create New User</h3>
-                <input type="text" onChange={handleUserName} placeholder='Your Name' required />
+                <input type="text" onChange={handleUserName} placeholder='Your Name' required /> <br />
+                <button onClick={handleSetTitle}>Submit</button>
+                <br /><br />
+                <form>
+                    <input type="email" name="email" id='email' placeholder='Your Email' required /> <br />
+                    <button onClick={handleAddUser} type='submit'>Add User</button>
+                </form>
             </div>
         </div>
     );
