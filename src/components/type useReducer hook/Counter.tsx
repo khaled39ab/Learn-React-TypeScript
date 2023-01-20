4b05770ab3e65e1621386eeb4a527180ddb31d35
khaled39ab <khaled39ab@gmail.com>
2023-01-20 23:58:35 +0600

    basic useReducer hook

--- src/components/type useReducer hook/Counter.tsx
+++ src/components/type useReducer hook/Counter.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+
+    const [count, setCount] = useState(0);
     return (
         <div>
             <h2>Type useReducer Hook</h2>
+
+            <div>
+                <h3>Count: {0}</h3>
+                <button>Increment</button>
+                <button>Reset</button>
+                <button>Decrement</button>
+            </div>
         </div>
     );
 };
