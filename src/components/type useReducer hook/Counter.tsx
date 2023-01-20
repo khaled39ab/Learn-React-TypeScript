c3f16a536676e9293d3ad2ca55fcbef4577434d7
khaled39ab <khaled39ab@gmail.com>
2023-01-21 00:18:09 +0600

    added type of state action reducer etc

--- src/components/type useReducer hook/Counter.tsx
+++ src/components/type useReducer hook/Counter.tsx
@@ -1,8 +1,41 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+const INCREMENT = 'INCREMENT';
+const RESET = 'RESET';
+const DECREMENT = 'DECREMENT';
+
+const initialState = {
+    count: 0
+}
+type counterState = {
+    count: number
+}
+
+type incrementType = { type: "INCREMENT" };
+type resetType = { type: "RESET" };
+type decrementType = { type: "DECREMENT" };
+
+type counterActionType = incrementType | resetType | decrementType;
+
+const reducer = (state: counterState, action: counterActionType) => {
+    switch (action.type) {
+        case INCREMENT:
+            return { count: state.count + 1 }
+
+        case RESET:
+            return { count: 0 }
+
+        case DECREMENT:
+            return { count: state.count - 1 }
+
+        default:
+            throw new Error();
+    }
+}
 
 const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [count, setCount] = useState(0);
     return (
         <div>
             <h2>Type useReducer Hook</h2>
